0daebb7b11723fc22f85557c3a0d90d3049205bb
Kevin Leung MBP <kkleung89@gmail.com>
2012-02-27 18:16:59 -0800

    putting in the turk code

--- experiment/stimuli_generation/generate_trials.py
+++ experiment/stimuli_generation/generate_trials.py
@@ -2,6 +2,12 @@ import string
 import random
 import math
 
+short_list = True
+num_stimuli = 100
+
+short_adjs = [line.rstrip('\n') for line in open('adj-small2.txt', 'r').readlines()]
+short_mods = [line.rstrip('\n') for line in open('mod-small2.txt', 'r').readlines()]
+
 lines = [line.rstrip('\n') for line in open('web1t-contextual-rescaling-pmi.csv', 'r').readlines()]
 
 mods = lines[0].replace('"', '').split(',')[1:]
@@ -32,7 +38,6 @@ num_adj = len(values)
 #print str(num_adj) + ' ' + str(len(adjs))
 
 used_stimuli = set([])
-num_stimuli = 100
 my_count = 0
 
 fout = open('stimuli.js', 'w')
@@ -50,25 +55,25 @@ def add_stimuli(f, cur_max, my_count):
 		if not f(values[adj][mod]): 
 			continue
 			
+		if short_list and not(mods[mod] in short_mods and adjs[adj] in short_adjs):
+			continue
+			
 		used_stimuli.add(ser)
-		fout.write('{{"trial": {0}, "isCatch": 0, "phrase": "This is {1} {2}."}}, // {3}\n'.format(my_count, mods[mod], adjs[adj], values[adj][mod]))
+		fout.write('{{"trial": {0}, "isCatch": 0, "phrase": "{1} {2}"}}, // {3}\n'.format(my_count, mods[mod], adjs[adj], values[adj][mod]))
 
 		cur_count += 1
 		my_count += 1
 	
 	return my_count
 	
-my_count = add_stimuli(lambda x: math.floor(x) <= -4, 10, my_count)
-my_count = add_stimuli(lambda x: math.floor(x) == -3, 10, my_count)
-my_count = add_stimuli(lambda x: math.floor(x) == -2, 10, my_count)
-my_count = add_stimuli(lambda x: math.floor(x) == -1, 10, my_count)
-my_count = add_stimuli(lambda x: x == 0, 5, my_count)
-my_count = add_stimuli(lambda x: x != 0 and math.floor(x) == 0, 5, my_count)
-my_count = add_stimuli(lambda x: math.floor(x) == 1, 10, my_count)
-my_count = add_stimuli(lambda x: math.floor(x) == 2, 10, my_count)
-my_count = add_stimuli(lambda x: math.floor(x) == 3, 10, my_count)
-my_count = add_stimuli(lambda x: math.floor(x) == 4, 10, my_count)
-my_count = add_stimuli(lambda x: math.floor(x) >= 5, 10, my_count)
+my_count = add_stimuli(lambda x: math.floor(x) <= -3, num_stimuli * .15, my_count)
+my_count = add_stimuli(lambda x: math.floor(x) == -2, num_stimuli * .15, my_count)
+my_count = add_stimuli(lambda x: math.floor(x) == -1, num_stimuli * .10, my_count)
+my_count = add_stimuli(lambda x: x == 0, num_stimuli * .10, my_count)
+my_count = add_stimuli(lambda x: x != 0 and math.floor(x) == 0, num_stimuli * .10, my_count)
+my_count = add_stimuli(lambda x: math.floor(x) == 1, num_stimuli * .10, my_count)
+my_count = add_stimuli(lambda x: math.floor(x) == 2, num_stimuli * .15, my_count)
+my_count = add_stimuli(lambda x: math.floor(x) == 3, num_stimuli * .15, my_count)
 
 
 	
