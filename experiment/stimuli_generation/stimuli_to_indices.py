89943b24e2deb05b4485f001c7290f35deb44346
Kevin Leung MBP <kkleung89@gmail.com>
2012-03-12 22:01:18 -0700

    passing the indices too

--- experiment/stimuli_generation/stimuli_to_indices.py
+++ experiment/stimuli_generation/stimuli_to_indices.py
@@ -5,20 +5,20 @@ import math
 short_list = True
 num_stimuli = 100
 
-folder = '../../data/word_sets/kennedy_extended/'
+folder = '../../data/word_sets/kennedy/'
 adj_file = folder + 'adj.txt'
 mod_file = folder + 'mod.txt'
 adjs = [line.rstrip('\n') for line in open(adj_file, 'r').readlines()]
 mods = [line.rstrip('\n') for line in open(mod_file, 'r').readlines()]
 
-lines = [line.rstrip('\n') for line in open('experiment_2/stimuli_exhaustive.csv', 'r').readlines()]
+lines = [line.rstrip('\n') for line in open('experiment_2/stimuli.csv', 'r').readlines()]
 
 fout = open(folder + 'indices.csv', 'w')
 
 for line in lines:
 	words = line.split(',')
-	mod = mods.index(words[0]) + 1
-	adj = adjs.index(words[1]) + 1
+	mod = mods.index(words[1].lstrip()) + 1
+	adj = adjs.index(words[0].lstrip()) + 1
 	fout.write('{0},{1}\n'.format(adj, mod))
 	
 fout.close()
